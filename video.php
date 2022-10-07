d50d89833206a7a77c0f031d15bb8d8e1fda8276
ILJX <39075977+ILJX@users.noreply.github.com>
2022-10-08 02:21:14 +0800

    Update video.php

--- video.php
+++ video.php
@@ -1,18 +1,5 @@
 <?php
 
-function read(...$filelist) {
-    $list = [];
-    foreach ($filelist as $file) {
-        $handle = fopen($file, 'r');
-        while (($line = fgets($handle)) !== false) {
-            array_push($list, trim($line));
-        }
-        fclose($handle);
-    }
-    return $list;
-}
-
-$list = read('ks.txt');
-$url = $list[array_rand($list)];
+$url = 'http://ali-cdn.kwai.net/upic/2016/07/16/18/BMjAxNjA3MTYxODAzMTNfMTY3Mjc0OTkwXzg5NjMzNjAyNV8yXzM=.mp4';
 
 header("Location: {$url}");
