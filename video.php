ffda0aa6c5fc5a20ed3aab4304e190a3fbe73801
ILJX <39075977+ILJX@users.noreply.github.com>
2022-10-08 07:51:04 +0800

    Update video.php

--- video.php
+++ video.php
@@ -1,5 +1,5 @@
 <?php
 
-$url = 'http://ali-cdn.kwai.net/upic/2016/07/16/18/BMjAxNjA3MTYxODAzMTNfMTY3Mjc0OTkwXzg5NjMzNjAyNV8yXzM=.mp4';
+$url = 'http://ali-cdn.kwai.net/upic/2017/09/27/12/BMjAxNzA5MjcxMjM3NTlfMjcwMjc0ODI0XzMzNjA0NjY0NjJfMV8z_b.mp4';
 
 header("Location: {$url}");
